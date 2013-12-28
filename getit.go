a6906ff484e04809bd7484e5965e2ba403715ecc
Oleg Pudeyev <oleg@bsdpower.com>
2013-12-28 05:06:02 -0500

    Fetch the schedules

--- getit.go
+++ getit.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+  "os"
   "fmt"
+  "io"
   "io/ioutil"
   "log"
   "net/http"
@@ -9,6 +11,41 @@ import (
   "path"
   "code.google.com/p/go.net/html"
 )
+
+func fetch(url string) {
+  res, err := http.Get(url)
+  if err != nil {
+    log.Fatal(err)
+  }
+  if err != nil {
+    log.Fatal(err)
+  }
+  // http://stackoverflow.com/questions/1821811/how-to-read-write-from-to-file
+  fo, err := os.Create(path.Base(url))
+  if err != nil {
+    panic(err)
+  }
+  defer func() {
+    if err := fo.Close(); err != nil {
+      panic(err)
+    }
+  }()
+  buf := make([]byte, 65536)
+  for {
+    n, err := res.Body.Read(buf)
+    if err != nil && err != io.EOF {
+      panic(err)
+    }
+    if n == 0 {
+      break
+    }
+    
+    if _, err := fo.Write(buf[:n]); err != nil {
+      panic(err)
+    }
+  }
+  res.Body.Close()
+}
 
 func main() {
   start := "http://www.panynj.gov/path/full-schedules.html"
@@ -31,8 +68,12 @@ func main() {
           if string(key) == "href" {
             v := string(value)
             if strings.HasPrefix(v, "schedules/") {
-              url := path.Join(path.Dir(start), v)
+              fuckedurl := path.Join(path.Dir(start), v)
+              // yep, hack it
+              // thx go for making me rename the variable
+              url := strings.Replace(fuckedurl, ":/", "://", 1)
               fmt.Printf("%s\n", url)
+              fetch(url)
             }
           }
           if !more {
